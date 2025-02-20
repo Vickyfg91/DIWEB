8aede35db5a353f252865cb03bbba9d377098b23
Vickyfg91 <vfergon346@g.educaand.es>
2025-02-20 14:33:14 +0100

    Comienzo Slider

--- TEMA4/webJUSTFAB/webJUSTFAB/jq/script-justfab.js
+++ TEMA4/webJUSTFAB/webJUSTFAB/jq/script-justfab.js
@@ -37,23 +37,6 @@ $(function(){
     });
 });
 
-/*Cada opción del submenú se muestra con un efecto y al mostrar una opción se ocultan todas las demás.*/
-/*No sale porque se queda pillado y vuelve a mostrar la lista y no cambia los iconos*/
-$(function(){
-    $("#menu > li > a").on("click", function(){
-        $("#menu > li > ul").slideUp();
-        console.log("ole1");
-        
-        $("#menu > li > a span i").removeClass("fa-angle-up").addClass("fa-angle-down");
-        console.log("ole3");
-        /*Lo mejor seria cambiar la clase donde hayas echo click cambia a fa-angle-up"*/
-        if (!$(this).siblings("ul").is(":visible")) {
-            $(this).siblings("ul").slideDown();
-            $(this).find("span i").removeClass("fa-angle-down").addClass("fa-angle-up");
-        }
-    });
-});
-
 
 /*Cuando haces un poco de scroll sobre la página debe aparecer con el efecto que consideres más apropiado, la cabecera de la página fijada en la parte superior.*/
 $(function(){
@@ -68,23 +51,41 @@ $(function(){
 });
 
 /*Al posicionar el ratón sobre la imagen de un producto debe aparecer otra imagen del mismo producto. Cuando el ratón deja de estar sobe la imagen aparece la imagen inicial. */
-
-$(function(){
-    $(".product-image").on("mouseenter", function(){
-        $(this).attr("src", "img/zapato1-1.jpg");
+/*$(function(){
+    $("main article .product-image").on("mouseenter", function(){
+        $(this).attr("src", $(this).attr("src").replace(".jpg", "-1.jpg"));
     }).on("mouseleave", function(){
-        $(this).attr("src", "img/zapato1.jpg");
+        $(this).attr("src", $(this).attr("src").replace("-1.jpg", ".jpg"));
+    });
+});*/
+$(function(){
+    $('article.item').on('mouseenter', function(){
+        $(this).finish().find('img').attr('src', $(this).find('img').attr('src').replace('.jpg', '-1.jpg'));
+    });
+    $('article.item').on('mouseleave', function(){
+        $(this).finish().find('img').attr('src', $(this).find('img').attr('src').replace('-1.jpg', '.jpg'));
     });
 });
+
+
+/*Cada opción del submenú se muestra con un efecto y al mostrar una opción se ocultan todas las demás.*/
 $(function(){
-    // Cambiar la imagen al pasar el ratón sobre ella en todos los artículos del main
-    $("main article .product-image").on("mouseenter", function(){
-        const originalSrc = $(this).attr("src");
-        const newSrc = originalSrc.replace(".jpg", "-1.jpg");
-        $(this).attr("src", newSrc);
-    }).on("mouseleave", function(){
-        const newSrc = $(this).attr("src");
-        const originalSrc = newSrc.replace("-1.jpg", ".jpg");
-        $(this).attr("src", originalSrc);
+
+    $("#menu > li > a").on("click", function(){
+        $("#menu > li > ul").finish().slideUp();
+        $("#menu > li > a span i").finish().removeClass("fa-angle-up").addClass("fa-angle-down");
+        if (!$(this).siblings("ul").is(":visible")) {
+            $(this).siblings("ul").slideDown();
+            $(this).find("span i").removeClass("fa-angle-down").addClass("fa-angle-up");
+        }
     });
+
+    $(document).on("click", function() {
+        $("#menu > li > ul").finish().slideUp();
+        })
+        $("#menu > li > a span i").finish().removeClass("fa-angle-up").addClass("fa-angle-down");
+        if (!$(this).siblings("ul").is(":visible")) {
+            $(this).siblings("ul").slideDown();
+            $(this).find("span i").removeClass("fa-angle-down").addClass("fa-angle-up");
+        }
 });
